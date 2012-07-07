37fc28572433ae6a5a026cffd6522250df10008d
Shane <shane@shane-ubuntu.(none)>
2012-07-06 19:58:45 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krooma::Application.config.secret_token = '529e640ff8a8049616b7835d1eb5d941284c59a911c80f65803bb35b54c436cdf6162cf4220d3e8ee2102574d9823f7587d9a70a716ea9d0a78a5b1ca927db9c'
